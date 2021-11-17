3d6b236e30863af084834357feb85e5ba28ac00e
Juliusstr <julius@struwing.dk>
2021-11-17 23:45:54 +0100

    test main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamDevice;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+        BufferedImage image = webcam.getImage();
+        ImageIO.write(image, "PNG", new File("test.png"));
+
     }
 }
