fac801402b0a9517a96fb61ae8a2d947069356bc
Juliusstr <julius@struwing.dk>
2021-11-18 16:27:38 +0100

    henter webcams og deres opløsninger

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,31 @@
 package com.company;
 
 import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamDevice;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Webcam webcam = Webcam.getDefault();
+        List<Webcam> webcamlist = Webcam.getWebcams();
+        for (int i = 0; i < webcamlist.size(); i++) {
+            System.out.println(i + " " + webcamlist.get(i).getName());
+            System.out.println("opløsninger:");
+            for (int j = 0; j < webcamlist.get(i).getViewSizes().length; j++) {
+                System.out.println("   " + webcamlist.get(i).getViewSizes()[j].width + " X " + webcamlist.get(i).getViewSizes()[j].height);
+            }
+
+        }
+        /*Webcam webcam = webcamlist.get(1);
         webcam.open();
         BufferedImage image = webcam.getImage();
         ImageIO.write(image, "PNG", new File("test.png"));
-
+        */
     }
 }
