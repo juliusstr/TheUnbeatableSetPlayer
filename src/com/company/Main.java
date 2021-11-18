f646f3db3fa5fda4127aa68fc1ad04cfb34072c0
Juliusstr <julius@struwing.dk>
2021-11-18 20:06:51 +0100

    master now uptodate on ImageHandler.class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, PosException {
 
-        final double radians = Math.toRadians(90);
+        /*final double radians = Math.toRadians(90);
         Webcam webcam = Webcam.getWebcams().get(1);
         webcam.setViewSize(new Dimension(640,480));
         webcam.open();
@@ -20,7 +20,12 @@ public class Main {
         webcam.close();
         image = ImageHandler.rotateImage(image, 0);
 
-        ImageIO.write(image, "PNG", new File("test.png"));
+        ImageIO.write(image, "PNG", new File("test.png"));*/
+
+        BufferedImage bacgeoud = ImageIO.read(new File("test.png"));
+        BufferedImage card = ImageIO.read(new File("test1.png"));
+        BufferedImage image = ImageHandler.mergeBackgroudAndCardPicture(bacgeoud,card, 200,10);
+        ImageIO.write(image, "PNG", new File("test2.png"));
 
     }
 }
