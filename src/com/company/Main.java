461563f84a3d9f9266df7ab76ba7dd94cf8035b5
Juliusstr <julius@struwing.dk>
2021-11-18 20:45:21 +0100

    added OpenCV as a lib

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
 import com.github.sarxos.webcam.Webcam;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -22,10 +25,14 @@ public class Main {
 
         ImageIO.write(image, "PNG", new File("test.png"));*/
 
-        BufferedImage bacgeoud = ImageIO.read(new File("test.png"));
+        /*BufferedImage bacgeoud = ImageIO.read(new File("test.png"));
         BufferedImage card = ImageIO.read(new File("test1.png"));
         BufferedImage image = ImageHandler.mergeBackgroudAndCardPicture(bacgeoud,card, 200,10);
-        ImageIO.write(image, "PNG", new File("test2.png"));
+        ImageIO.write(image, "PNG", new File("test2.png")); */
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(5, 5, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
 
     }
 }
